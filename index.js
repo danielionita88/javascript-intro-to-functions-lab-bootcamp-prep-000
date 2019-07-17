bc5c70f10fb5e3b8cf0349a234f3ff496a73730b
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-07-17 15:50:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   
- if (string.toLowerCase()) {return "I can't hear you!"}
- if (string.toUpperCase()) {return "YES INDEED!"}
+ if (string == "hello!") {return "I can't hear you!"}
+ if (string == "HELLO!") {return "YES INDEED!"}
  return "I love you, too."
 }
